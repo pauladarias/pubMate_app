6e94389136e1fea8a4b75a5d89383ea8aaa9af13
pauladarias <pauladarias@gmail.com>
2020-12-22 09:43:57 +0000

    Finish styling feed/post components

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
+        <Widjet />
       </div>
 
     </div>
--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,7 @@
 import { Avatar } from '@material-ui/core'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline'
+import NearMeIcon from '@material-ui/icons/NearMe'
 import React from 'react'
 import "./Post.css"
 
@@ -16,9 +19,27 @@ export default function Post({ profilePic, image, username, timestamp, message }
       <div className="post__bottom">
       <p>{message}</p>
       </div>
+
       <div className="post__image">
         <img src={image} alt=""/>
       </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <FavoriteIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+      </div>
+
+
     </div>
   )
 }
